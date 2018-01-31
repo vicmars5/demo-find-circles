61800b4242438ec08e5187f476fbb175317eba91
Victor Diaz <mars.vic5@gmail.com>
2018-01-30 23:56:49 -0600

    start up

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'sem-algoritmia'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
